643b568adca2045b174f18c60ae242025850548a
SH Sadeghzadeh <46283990+SHSADEGH@users.noreply.github.com>
2019-11-07 17:19:44 +0330

    Updates

---
+++ precache-manifest.b194830d46dce268670a068478ce8feb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bacb33d196e43fcdaa10821c0d6de48",
+    "url": "/smsadegh82/my_resume2/index.html"
+  },
+  {
+    "revision": "ef9d09bf20b0bcca0327",
+    "url": "/smsadegh82/my_resume2/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5811e72953cb4b13364f",
+    "url": "/smsadegh82/my_resume2/static/css/main.9f56fb31.chunk.css"
+  },
+  {
+    "revision": "ef9d09bf20b0bcca0327",
+    "url": "/smsadegh82/my_resume2/static/js/2.5e4284aa.chunk.js"
+  },
+  {
+    "revision": "5811e72953cb4b13364f",
+    "url": "/smsadegh82/my_resume2/static/js/main.e3e5454f.chunk.js"
+  },
+  {
+    "revision": "a8335f197586a41c5c0a",
+    "url": "/smsadegh82/my_resume2/static/js/runtime~main.db034115.js"
+  },
+  {
+    "revision": "66a0d84ca506a99637768fb15819e077",
+    "url": "/smsadegh82/my_resume2/static/media/Vmarine.66a0d84c.jpg"
+  },
+  {
+    "revision": "fa004d0fba99dcf9abe53ea0c2264549",
+    "url": "/smsadegh82/my_resume2/static/media/aguaDeLuz.fa004d0f.png"
+  }
+]);
